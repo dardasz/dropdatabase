149e2f256d5c9e18076b66b0602eb3f7ec36abf2
Papilion Dániel <papiliond.94@gmail.com>
2017-02-03 16:30:33 +0100

    Obstacles
    ...

---
+++ SnakeOfTheHill/Assets/generateObstacle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generateObstacle : MonoBehaviour {
+
+    // Use this for initialization
+    float x;
+    float y;
+    float z;
+    Vector3 pos;
+
+    void Start()
+    {
+        x = Random.Range(-18, 18);
+        z = Random.Range(-18, 18);
+
+        pos = new Vector3(x, 0, z);
+        transform.position = pos;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
+
+/*
+ 
+
+
+
+ * */
