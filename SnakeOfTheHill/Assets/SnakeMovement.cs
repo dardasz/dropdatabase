db6fba474d79276b1c174042a1fa0742a951b8bf
dardasz <fabiankrisztian@indamail.hu>
2017-02-03 16:31:24 +0100

    Snakeeeee
    asd

---
+++ SnakeOfTheHill/Assets/SnakeMovement.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeMovement : MonoBehaviour {
+
+    public List<GameObject> Snake  =  new List<GameObject>();
+    int Tictime;
+    public int TicTimeLimit;
+    public string dir = "w";
+    public Vector3 pos;
+
+    string w = "w";
+    string a = "a";
+    string s = "s";
+    string d = "d";
+
+    void Inputs()
+    {
+        if (Input.GetKeyDown(w))
+        {
+            dir = "w";
+            Tictime = TicTimeLimit;
+        }
+        if (Input.GetKeyDown(s))
+        {
+            dir = "s";
+            Tictime = TicTimeLimit;
+        }
+
+        if (Input.GetKeyDown(a))
+        {
+            dir = "a";
+            Tictime = TicTimeLimit;
+        }
+
+        if (Input.GetKeyDown(d))
+        {
+            dir = "d";
+            Tictime = TicTimeLimit;
+
+        }
+    }
+
+    void addtosnake() {
+        GameObject part = Instantiate(Resources.Load<GameObject>("part"))as GameObject;
+        if (!Snake.Contains(part)) {
+            Snake.Add(part);
+        }
+    }
+
+    void Updatesnake() {
+        Tictime++;
+
+        for(int i = Snake.Count -1; i > 0 ; i--) {
+            if (i > 0) {
+                Snake[i].transform.position = Snake[i - 1].transform.position;
+            }
+        }
+        Snake[0].transform.position = transform.position;
+
+        if (Tictime >= TicTimeLimit) {
+
+            switch (dir) {
+                case "w":
+                    pos.z++;
+                    break;
+
+                case "a":
+                    pos.x--;
+                    break;
+
+                case "s":
+                    pos.z--;
+                    break;
+
+                case "d":
+                    pos.x++;
+                    break;
+            }
+
+            transform.position = pos;
+
+            Tictime = 0;
+        }
+    }
+
+                // Use this for initialization
+    void Start () {
+        for (int i = 0; i<10 ; i++) {
+            addtosnake();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Inputs();
+        Updatesnake();
+	}
+}
